ee26af0083cb58fac1e6c0614454a962c30087bc
SiggyD <Dave.Sigmundson+git@gmail.com>
2016-03-01 18:10:32 -0500

    now with dbs

--- newuser.php
+++ newuser.php
@@ -53,29 +53,29 @@
 				<p class="lead">Register</p>
 			</div>
 			<div align="center" class="panel panel-primary">
-				<div align="left" class="panel-body"><strong style="font-size:2em;">Register<strong></div>
+				<div align="left" class="panel-body"><strong ">Register<strong></div>
 					
 					<table class="table-condensed"><tr><td>
-						<form class="registration-form" role="form" action="newuser.php" method="post">
+						<form class="registration-form" role="form" action="newuser.php" method="POST">
 							<div class="form-group">
-								<input type="text" name="username" placeholder="Username..." class="form-control" id="username">
+								<input type="text" name="username" placeholder="Username..." class="form-control" id="username" required autofocus>
 							</div>
 							<div class="form-group">
-								<input type="email" name="email" placeholder="Email..." class="form-control" id="email">
+								<input type="email" name="email" placeholder="Email..." class="form-control" id="email"required>
 							</div>
 							<div class="form-group">
-								<input type="password" name="password" placeholder="Password..." class="form-control" id="password">
+								<input type="password" name="password" placeholder="Password..." class="form-control" id="password"required>
 							</div>
 							<div class="form-group">
-								<input type="password" name="passwordConf" placeholder="Retype Password..." class="form-control" id="passwordConf">
+								<input type="password" name="passwordConf" placeholder="Retype Password..." class="form-control" id="passwordConf"required>
 							</div>
 							<button type="submit" class="btn btn-primary">Submit</button>
 						</form></td>
 						<td>
 							<?php
-								
 								$valid;
-								if ($_SERVER['REQUEST_METHOD'] == 'POST') //small check to ensure request was post, hopefully indicating form data
+								#if ($_SERVER['REQUEST_METHOD'] == 'POST') //small check to ensure request was post, hopefully indicating form data
+								if ((!empty($_POST))) //small check to ensure request was post, hopefully indicating form data
 								{
 									$valid = 0;
 									//collect posted data & validate
@@ -84,38 +84,52 @@
 									$username = stripSpecial($username);
 									if (!preg_match("/^[a-zA-Z]{2,20}$/",$username))
 									{
-										echo "<div class=\"alert alert-danger\"><strong>Username must be between 5-20 characters</strong> </div>";
+										echo "<div class=\"alert alert-danger\">Username must be between 2-20 characters</div>";
 										$valid = 1;
 									}
-									//email		
+									//email	
+										
 									$email = trim($_POST["email"]);
 									$email = stripSpecial($email );
 									if (!preg_match("/^[a-zA-Z.-_]+\@[a-zA-Z-_]+\.[a-zA-Z\.]{2,3}$/",$email))
 									{
-										echo "<div class=\"alert alert-danger\"><strong>Please input a valid email</strong> </div>";
+										echo "<div class=\"alert alert-danger\">Please input a valid email</div>";
 										$valid = 1;
 									}
 									$email = trim($_POST["email"]);
 									//password
+									
 									$password = trim($_POST["password"]);
 									$passwordConf = trim($_POST["passwordConf"]);
 									if (empty($password) || empty($passwordConf)) 
 									{
-										echo "<div class=\"alert alert-danger\"><strong>Password can not be empty.</strong> </div>";
-										$valid = 1; 
-									}
-									if (strcmp($password,$passwordConf) != 0)
-									{
-										echo "<div class=\"alert alert-danger\"><strong>Passwords must match.</strong> </div>";
+										echo "<div class=\"alert alert-danger\">Password can not be empty.</div>";
 										$valid = 1; 
 									}
 									
-									//db do
-									if($valid == 0)
+									if (strcmp($password,$passwordConf) != 0)
 									{
+										echo "<div class=\"alert alert-danger\">Passwords must match.</div>";
+										$valid = 1; 
+									}
+									
+									////////////////////////////////db do
+									if($valid == 0)
+									{	
+										#$salt = base64_encode(openssl_random_pseudo_bytes(20));
+										$hash =  password_hash($password.$username,PASSWORD_DEFAULT);
+										#DEBUG INFO						
+										#echo "<br>username: ".$username;
+										#echo "<br>email: ".$email;
+										#echo "<br>hash: ".$hash;
+										#DEBUG INFO 
+										$db = pg_connect('host=localhost dbname=ssd user=sig password=ssd');
+										$insertStatement = "INSERT INTO piddle (username, email, passhash) VALUES ('" . $username . "','" . $email . "','" . $hash . "');";
+										//DO
 										$insertResult = pg_query($insertStatement);
 										echo "<script type='text/javascript'> alert('User Added!')</script>";
 									}
+									
 								}
 								function stripSpecial($string)	//get rid of comment chars.
 								{
@@ -136,12 +150,7 @@
 									$string = urlencode($string);
 									return $string;
 								}
-<<<<<<< HEAD
-								
-								
-								
-=======
->>>>>>> a95a0c793abac4f540eaf155c30b74432979ca01
+
 							?>
 						</td></tr></table></div>
 				</body>
