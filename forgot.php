1c539b842ee8e4527f9a6c3cc151f9755d898ab9
omalax <omalaxation@gmail.com>
2016-03-03 20:56:42 -0500

    UUHHH FUCKKKKKKKK

--- forgot.php
+++ forgot.php
@@ -26,29 +26,46 @@
 		{
 			$emaillog = $_POST['email'];
 			$file = '/var/www/log/security.log';
-			$db = pg_connect('host=localhost dbname=ssd user=sig password=ssd')
+			$db = pg_connect('host=localhost dbname=ssd user=omalax password=ssd')
 			or die("Can't connect to database".pg_last_error());
-			if (!pg_prepare($db,'login_select', 'SELECT * FROM piddle WHERE email = $1')) 
+			if (!pg_prepare($db,'login_select', 'SELECT * FROM piddle WHERE email = $1'))
 			{
 				die("Can't prepare" . pg_last_error());
 			}
-						$email = $_POST["email"];
+			$email = $_POST["email"];
 			$password = $_POST["password"];
 			$result = pg_execute($db, 'login_select', array($email));
 			$row = pg_fetch_assoc($result);
 #db stuff is available
-			
-			# after user fetch 
+
+			# after user fetch
 # check 1, user exists
 			if ($email == $row['email']) # check if user was returned
 			{
-				file_put_contents('/var/www/log/security.log',date(DATE_RFC2822).": User ".$row['username']." reset password from ".$_SERVER['REMOTE_ADDR']." - RESET FAIL\n", FILE_APPEND);
 				#continue
+				$db = pg_connect('host=localhost dbname=ssd user=omalax password=ssd');
+				if (!pg_prepare($db, 'get_id', 'SELECT id FROM piddle WHERE email = $1')) {
+						die("Can't prepare" . pg_last_error());
+				}
+				$insertStatement = pg_execute($db, 'get_id', array($email));
+				#echo $insertStatement;
+				$row = pg_fetch_assoc($insertStatement);
+				$id = $row['id'];
+				$passtoken = bin2hex(openssl_random_pseudo_bytes(32));
+
+				if (!pg_prepare($db, 'new_token_insert', 'UPDATE token SET locktoken = $1 WHERE id = $2 RETURNING id')) {
+						die("Can't prepare" . pg_last_error());
+				}
+				$insertToken = pg_execute($db, 'new_token_insert', array($passtoken, $id));
+				#$passtokenlogstring = "https://piddlebox.xyz/reg.php?id=".$id."&regtoken=".$passtoken;
+				$passtokenlogstring = "http://localhost/reset.php?id=".$id."&locktoken=".$passtoken;
+				file_put_contents('/var/www/log/security.log',date(DATE_RFC2822).": User ".$row['username']." reset password from ".$_SERVER['REMOTE_ADDR']." - RESET LINK - ".$passtokenlogstring."\n", FILE_APPEND);
+				echo "<script type='text/javascript'> alert('Reset Sent!')</script>";
 			}
 			else
 			{
 				echo "<div class=\"alert alert-danger\">Invalid Username, this has been reported. </div>";
-				file_put_contents($file,date(DATE_RFC2822).": User attempted to reset unknown account from ".$_SERVER['REMOTE_ADDR']." - PASS RESET\n", FILE_APPEND);
+				file_put_contents($file,date(DATE_RFC2822).": User attempted to reset unknown account from ".$_SERVER['REMOTE_ADDR']." - RESET FAIL\n", FILE_APPEND);
 				exit();
 				#exit, no log
 			}
